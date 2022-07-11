c168f87a2e5f4514c59004ef8bf83759f92487a7
juanc22z <juan.isaza@giproyectos.com>
2022-07-11 03:26:14 +0000

    Cap

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,9 +1,9 @@
 
 const cds = require('@sap/cds')
 
-/**
+/******
  * Implementation for Risk Management service defined in ./risk-service.cds
- */
+ ******/
 module.exports = cds.service.impl(async function() {
     this.after('READ', 'Risks', risksData => {
         const risks = Array.isArray(risksData) ? risksData : [risksData];
